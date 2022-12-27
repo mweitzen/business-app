9fee6c7182a383a1bf30d51b80739a030d3b99cd
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2022-12-27 14:22:25 -0700

    fixing prisma seeding logic

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,31 +1,75 @@
-import { assets, users } from "./seed-data";
 import { PrismaClient } from "@prisma/client";
+import { assets, users, assetAssignments } from "./seed-data";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const newUsers = users.map(async (user) => {
-    const data = await prisma.user.create({
-      data: user,
-    });
-    return data;
-  });
+  /*
+   * CREATE USERS
+   */
+  const newUsers = await Promise.all(
+    users.map(async (user) => {
+      const data = await prisma.user.create({
+        data: user,
+      });
+      return data;
+    })
+  );
 
-  assets.forEach(async (asset) => {
-    await prisma.asset.create({
-      data: {
-        ...asset,
-        purchase: {
-          create: {
-            orderNumber: "",
-            purchaseDate: new Date(),
-            purchasePrice: 300,
-            purchasedFrom: "amazon.com",
+  /*
+   * CREATE ASSETS AND ASSIGN USERS
+   */
+  newUsers &&
+    assets.forEach(async (asset) => {
+      const assignedTo = assetAssignments[asset.name] ?? null;
+
+      if (assignedTo) {
+        await prisma.asset.create({
+          data: {
+            ...asset,
+            status: "assigned",
+            assignedTo: {
+              connect: {
+                email: assignedTo,
+              },
+            },
+            ownerHistory: {
+              create: {
+                owner: {
+                  connect: {
+                    email: assignedTo,
+                  },
+                },
+              },
+            },
+            // creating dummy purchase at the moment
+            purchase: {
+              create: {
+                orderNumber: "",
+                purchaseDate: new Date(),
+                purchasePrice: 300,
+                purchasedFrom: "amazon.com",
+              },
+            },
           },
-        },
-      },
+        });
+      } else {
+        await prisma.asset.create({
+          data: {
+            ...asset,
+            // creating dummy purchase at the moment
+            purchase: {
+              create: {
+                orderNumber: "",
+                purchaseDate: new Date(),
+                purchasePrice: 300,
+                purchasedFrom: "amazon.com",
+              },
+            },
+          },
+        });
+      }
     });
-  });
 }
 
 main()
