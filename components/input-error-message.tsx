61011830b6f70333e42aa28f69cbe3cc7c42d7e8
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2022-12-29 14:02:12 -0700

    fiddling cleaning

--- components/input-error-message.tsx
+++ components/input-error-message.tsx
@@ -2,8 +2,8 @@ import { FieldMetaProps } from "formik";
 
 const InputErrorMessage = ({ meta }: { meta: FieldMetaProps<any> }) => {
   return meta.touched && meta.error ? (
-    <span className="ml-2 text-xs font-medium text-purple-600">
-      {meta.error}
+    <span className="ml-2 mt-[1px] flex items-center text-xs font-medium text-purple-600">
+      * {meta.error}
     </span>
   ) : null;
 };
